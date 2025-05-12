9d364df58f385acf9b51a7ea5c473ad5c38d9ab6
doohaaa <engk5423@naver.com>
2025-05-12 15:39:01 +0900

    init commit

---
+++ src/main/java/com/example/calenderproject/entity/Plan.java
@@ -0,0 +1,18 @@
+package com.example.calenderproject.entity;
+
+import java.time.format.DateTimeFormatter;
+
+public class Plan {
+
+    private Long id;
+    private String writer;
+    private String contents;
+    private String password;
+    private DateTimeFormatter createdDate;
+    private DateTimeFormatter modifiedDate;
+
+
+
+
+
+}
